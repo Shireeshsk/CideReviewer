c471f294a03ac97bf4b75046933ff5addc8baf5e
Shireeshsk <poosashireeshkumar.2005@gmail.com>
2025-04-22 19:38:42 +0530

    Initial commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const airoutes= require('./routes/ai.routes')
 const app = express();
+const cors = require('cors');
+app.use(cors())
 app.use(express.json())
 app.use('/ai',airoutes);
 app.get('/',(req,res)=>{
